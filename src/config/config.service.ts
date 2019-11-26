3e008a211581ad3ba9a234f67a6bd86b34478a7f
Mauricio Oropeza <mauricio.3247@gmail.com>
2019-11-26 00:33:18 -0400

    change configService to use app with heroku

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -5,7 +5,7 @@ export class ConfigService {
   private readonly envConfig: Record<string, string>;
 
   constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath))
+    this.envConfig = process.env
   }
 
   get(key: string): string {
